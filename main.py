9f2f269c644af0b51cdef8601d46a76156bdb120
Darkdoomsday <lezardv@hotmail.com>
2023-09-04 16:54:40 -0500

    Commit

--- main.py
+++ main.py
@@ -10,18 +10,19 @@ search_box.send_keys("Cartas")
 search_button = driver.find_element(by=By.CSS_SELECTOR, value="#searchbox > button > span")
 search_button.click()
 
-articles = driver.find_elements(By.CSS_SELECTOR, "#center_column > ul")
+articles = driver.find_elements(By.CSS_SELECTOR, "#center_column > ul > li:nth-child(n) > div")
 
-#mongodb = MongoDriver()
+mongodb = MongoDriver()
 
 for card in articles:
     try:
-        title = card.find_element(By.CSS_SELECTOR, "div > div > div.center-block2.col-xs-4.col-xs-7.col-md-6 > h3").text
+        #image = card.find_element(By.CSS_SELECTOR, "div > div > div.left-block2.col-xs-4.col-xs-4.col-md-3 > div > a > img").image
+        title = card.find_element(By.CSS_SELECTOR, "div > div > div.center-block2.col-xs-4.col-xs-7.col-md-6 > h3 > a").text
         description = card.find_element(By.CSS_SELECTOR, "div > div > div.center-block2.col-xs-4.col-xs-7.col-md-6 > p").text
         price = card.find_element(By.CSS_SELECTOR, "div > div > div.right-block2.col-xs-4.col-xs-7.col-md-3 > div > div.content_price.col-xs-12.col-md-12 > span").text
         print(title)
         print(description)
-        print(f"${price}")
+        print(f"{price}")
 
         juegos = {
             "title": title,
@@ -31,7 +32,7 @@ for card in articles:
 
 
 
-        #mongodb.insert_record(record=juegos, username="cartas")
+        mongodb.insert_record(record=juegos, username="cartas")
 
         print("++++++++++++++++++++++++++++++++")
     except Exception as e:

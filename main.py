55953e3c96a60b844ac7d5eab6ab5acec7c776a5
Darkdoomsday <lezardv@hotmail.com>
2023-09-03 22:18:04 -0500

    Commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from db import MongoDriver
+
+driver = webdriver.Chrome()
+driver.get("https://juegosdelamesaredonda.com/")
+search_box = driver.find_element(by=By.CSS_SELECTOR, value="#search_query_top")
+search_box.send_keys("Cartas")
+
+search_button = driver.find_element(by=By.CSS_SELECTOR, value="#searchbox > button > span")
+search_button.click()
+
+articles = driver.find_elements(By.CSS_SELECTOR, "#center_column > ul")
+
+#mongodb = MongoDriver()
+
+for card in articles:
+    try:
+        title = card.find_element(By.CSS_SELECTOR, "div > div > div.center-block2.col-xs-4.col-xs-7.col-md-6 > h3").text
+        description = card.find_element(By.CSS_SELECTOR, "div > div > div.center-block2.col-xs-4.col-xs-7.col-md-6 > p").text
+        price = card.find_element(By.CSS_SELECTOR, "div > div > div.right-block2.col-xs-4.col-xs-7.col-md-3 > div > div.content_price.col-xs-12.col-md-12 > span").text
+        print(title)
+        print(description)
+        print(f"${price}")
+
+        juegos = {
+            "title": title,
+            "description": description,
+            "price": price
+        }
+
+
+
+        #mongodb.insert_record(record=juegos, username="cartas")
+
+        print("++++++++++++++++++++++++++++++++")
+    except Exception as e:
+        print(e)
+        print("++++++++++++++++++++++++++++++++")
+
+
+driver.close()
